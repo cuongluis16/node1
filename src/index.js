f34a28724dd339786cc1edb90825b702bcead4c9
cuongluis16 <200062pf@yse-c.net>
2021-11-22 11:17:51 +0900

    node

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const chalk = require('chalk');
+console.log(chalk.bgRed("Hello Chalk!"));
+console.log(chalk.bgGreen("Hello Chalk!"));
+
+let lat = process.argv[2]
+let lng = process.argv[3]
+
+const cities = require('cities');
+if (lat && lng) {
+    let city = cities.gps_lookup(lat, lng);
+    console.log(city);
+    
+}
